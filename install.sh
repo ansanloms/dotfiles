8e493516fe75d4f56e542da789185fc1d7f6a96a
ansanloms <16961622+ansanloms@users.noreply.github.com>
2019-04-04 20:24:01 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
 ln -sf ${SCRIPT_DIR}/.bashrc            ~/.bashrc
 ln -sf ${SCRIPT_DIR}/.bash_profile      ~/.bash_profile
 ln -sf ${SCRIPT_DIR}/.vimrc             ~/.vimrc
+ln -sf ${SCRIPT_DIR}/.vimrc.general     ~/.vimrc.general
+ln -sf ${SCRIPT_DIR}/.vimrc.search      ~/.vimrc.search
 ln -sf ${SCRIPT_DIR}/.vimrc.keymap      ~/.vimrc.keymap
 ln -sf ${SCRIPT_DIR}/.gitconfig         ~/.gitconfig
 ln -sf ${SCRIPT_DIR}/.ctrlp-launcher    ~/.ctrlp-launcher
