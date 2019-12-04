4cee064cc9fc2766ab640200a7f7d2572890f047
ansanloms <16961622+ansanloms@users.noreply.github.com>
2019-12-04 16:08:58 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,9 +6,6 @@ ln -sf ${SCRIPT_DIR}/.inputrc           ~/.inputrc
 ln -sf ${SCRIPT_DIR}/.bashrc            ~/.bashrc
 ln -sf ${SCRIPT_DIR}/.bash_profile      ~/.bash_profile
 ln -sf ${SCRIPT_DIR}/.vimrc             ~/.vimrc
-ln -sf ${SCRIPT_DIR}/.vimrc.general     ~/.vimrc.general
-ln -sf ${SCRIPT_DIR}/.vimrc.search      ~/.vimrc.search
-ln -sf ${SCRIPT_DIR}/.vimrc.keymap      ~/.vimrc.keymap
 ln -sf ${SCRIPT_DIR}/.gitconfig         ~/.gitconfig
 ln -sf ${SCRIPT_DIR}/.ctrlp-launcher    ~/.ctrlp-launcher
 
