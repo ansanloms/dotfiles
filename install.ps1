052560167a5dbda9298f0116fa81afb15f432e4c
ansanloms <16961622+ansanloms@users.noreply.github.com>
2020-07-17 22:45:41 +0900

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -6,10 +6,8 @@ $DOTBOT_DIR = "dotbot"
 $DOTBOT_BIN = "bin/dotbot"
 $BASEDIR = Convert-Path $(Split-Path $MyInvocation.InvocationName -Parent)
 
-$args_string = $args -join " "
-
 cd "${BASEDIR}"
 git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive
 git submodule update --init --recursive "${DOTBOT_DIR}"
 
-python "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "$args_string"
+python "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "$($args -join " ")"
