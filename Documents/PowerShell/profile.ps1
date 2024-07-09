383b445a35c26824d76c56b41310cafb07363210
ansanloms <ansanloms@users.noreply.github.com>
2024-07-09 16:41:05 +0900

    feat: profile.ps1 を更新。

--- Documents/PowerShell/profile.ps1
+++ Documents/PowerShell/profile.ps1
@@ -1,7 +1,6 @@
 $Env:LANG = "ja"
 $Env:EDITOR = "vim"
 $Env:LESSCHARSET = "UTF-8"
-$Env:FZF_DEFAULT_COMMAND = "rg --files --path-separator /"
 
 $Env:Path += ";" + (Join-Path $Env:homedrive  "\msys64\usr\bin")
 $Env:Path += ";" + (Join-Path $Env:homedrive  "\msys64\mingw64\bin")
@@ -11,8 +10,31 @@ $Env:Path += ";" + (Join-Path $Env:programfiles  "\Vim\vim91")
 # bash ライクな補完
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 
-# starship
+# Prompt に staship を使う
 Invoke-Expression (&starship init powershell)
 
+# PSReadLine
+Set-PSReadLineOption -Colors @{ InlinePrediction = $PSStyle.Foreground.BrightBlack }
+Set-PSReadlineOption -BellStyle None
+
+function eza-no-color() {
+  eza $(@("--color", "never"); $args)
+}
+
+function eza-ll() {
+  eza $(@("--color", "never", "--long"); $args)
+}
+
 # alias
-Set-Alias -Name vim -Value gvim
+if (Get-Command eza -errorAction SilentlyContinue) {
+  # 少なくとも gvim の terminal 上では ansi color エスケープシーケンスが上手く反映されないので。
+  Set-Alias -Name ls -Value eza-no-color
+  Set-Alias -Name ll -Value eza-ll
+} else {
+  Set-Alias -Name ll -Value Get-ChildItem
+}
+if (Get-Command bat -errorAction SilentlyContinue) {
+  Set-Alias -Name cat -Value bat
+}
+Set-Alias -Name vi -Value vim
+Set-Alias -Name open -Value Invoke-Item
