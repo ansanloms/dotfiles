35594c2d4c755ff7fe22eef17ad2969ceaf504e4
ansanloms <ansanloms@users.noreply.github.com>
2026-01-19 15:44:47 +0900

    fix: nvim

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -90,6 +90,7 @@ require("jetpack.packer").startup(function(use)
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
       vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope oldfiles" })
+      vim.keymap.set("n", "<leader>fs", builtin.git_status, { desc = "Telescope git status" })
       vim.keymap.set("n", "<leader>fl", launcher(require("config.launcher")), { desc = "Telescope launcher" })
     end,
   })
